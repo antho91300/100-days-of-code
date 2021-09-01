db7bf806331ae2f87eaad7ea367105025c6920f9
Anthony RICH <anthony.rich@free.fr>
2021-09-01 08:35:46 +0200

    test

---
+++ projects/todo-app/firestore.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCej-qwv55romNnFHZuvgC5vRnBQrACOO4",
+  authDomain: "todo-list-app-8e31f.firebaseapp.com",
+  databaseURL: "https://todo-list-app-8e31f-default-rtdb.firebaseio.com",
+  projectId: "todo-list-app-8e31f",
+  storageBucket: "todo-list-app-8e31f.appspot.com",
+  messagingSenderId: "862425908201",
+  appId: "1:862425908201:web:c782e4202cb2701359c178",
+  measurementId: "G-RN8WV02CX5",
+};
